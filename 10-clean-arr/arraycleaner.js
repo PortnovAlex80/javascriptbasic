aa2f29e579ed4b1f9dec3e2dd82d16ee9b41284b
PortnovAlex80 <savinopoint@ya.ru>
2023-02-10 19:21:26 +0300

    j deleted

--- 10-clean-arr/arraycleaner.js
+++ 10-clean-arr/arraycleaner.js
@@ -3,7 +3,7 @@ const arr = [0, 50, 25, 52 , 100, 37, 78, 25, 30];
 // manual filter
 function arrayFilter(array, deleteParamsFunction) {
     let filteredArray = [];
-    for (let  = 0; j < array.length; j++) {
+    for (let j = 0; j < array.length; j++) {
         deleteParamsFunction(array[j]) && filteredArray.push(array[j]);
     };
     return filteredArray;
