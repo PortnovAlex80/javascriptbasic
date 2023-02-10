04013e6415d1596a6d2e823cbfde45e2caf6c972
PortnovAlex80 <savinopoint@ya.ru>
2023-02-10 22:11:07 +0300

    correct methods and forrmatting

--- 15-task-object/taskobject.js
+++ 15-task-object/taskobject.js
@@ -6,22 +6,22 @@ const toDoList = {
             priority: 1
            }
     ],
-    addTask: function (task = {title: 'No title', id: 0, priority: 5}) { // default task if empty object
+    addTask(task = {title: 'No title', id: 0, priority: 5}) { // default task if empty object
         this.tasks.push(task);
     },
-    removeTaskById: function(id) {
+    removeTaskById(id) {
         const indexTaskByIdinArray = this.findTaskIndexById(id); //find task by id
         // deleted task by id
         (indexTaskByIdinArray) && this.tasks.splice(indexTaskByIdinArray,1);
         },
-    updateTaskById: function(taskForUpdate) { 
+    updateTaskById(taskForUpdate) { 
         //find task by id        
         let indexTaskByIdinArray = this.findTaskIndexById(taskForUpdate.id);
         if (!indexTaskByIdinArray) { return false; // if not found - return
         } 
         this.tasks[indexTaskByIdinArray] = taskForUpdate;
     },
-    sortTasksByPriority: function(order = 'DSC') {
+    sortTasksByPriority(order = 'DSC') {
         if (!this.tasks.length) return false; // if task is empty return false
         let order = (order !== 'DSC') ? true : false // sort order . is nor DSC means ASC
         return        order 
@@ -30,7 +30,7 @@ const toDoList = {
         : 
         this.tasks.sort( (a, b) => a.priority < b.priority )     
     },
-    findTaskIndexById: function(id) { // find task index in tasks array
+    findTaskIndexById(id) { // find task index in tasks array
         let indexTaskByIdinArray = this.tasks.findIndex(el => el.id === id ); 
         return (indexTaskByIdinArray <0) ? false : indexTaskByIdinArray;
     },
